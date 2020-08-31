0b12fa59d28d1f0878547266d33605950d100f8a
roboblox <supeecyborg@gmail.com>
2020-08-31 19:13:43 -0400

    finished line-decoration

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "./Todo.css";
 const Todo = (props) => {
   return (
     <div
       className={`todo${props.todo.completed ? " completed" : ""}`}
       onClick={() => props.toggleTodo(props.todo.id)}
     >
-      <p>{props.todo.task}</p>
+      {<p>{props.todo.task}</p>}
     </div>
   );
 };
